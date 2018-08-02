5de3081576039956cd43332407b6912fd2c97a9d
cvsooraj3 <35387667+cvsooraj3@users.noreply.github.com>
2018-08-02 15:25:32 -0700

    Update OI.java

--- src/org/usfirst/frc/team115/robot/OI.java
+++ src/org/usfirst/frc/team115/robot/OI.java
@@ -12,31 +12,38 @@ package org.usfirst.frc.team115.robot;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	//// CREATING BUTTONS
-	// One type of button is a joystick button which is any button on a
-	//// joystick.
-	// You create one by telling it which joystick it's on and which button
-	// number it is.
-	// Joystick stick = new Joystick(port);
-	// Button button = new JoystickButton(stick, buttonNumber);
+	Joystick operaterConsole;
+	JoystickButton elevateToSwitchLow;
+	JoystickButton elevateToSwitchHigh;
+	JoystickButton zeroElevator;
+	JoystickButton levelTwoElevate;
+	JoystickButton levelThreeElevate;
+	JoystickButton manualElevate;
+	public OI(){
+		 operaterConsole = new Joystick(2);
+			//switch
+			elevateToSwitchLow = new JoystickButton(operaterConsole, 1);
+			elevateToSwitchHigh = new JoystickButton(operaterConsole, 2);
+			//ground operations	
+			zeroElevator = new JoystickButton(operaterConsole, 3);
+			//Intakes at various heights
+			levelTwoElevate = new JoystickButton(operaterConsole, 4);
+			levelThreeElevate = new JoystickButton(operaterConsole, 5);
+			manualElevate = new JoystickButton(operaterConsole, 6);
 
-	// There are a few additional built in buttons you can use. Additionally,
-	// by subclassing Button you can create custom triggers and bind those to
-	// commands the same as any other Button.
+		
+		elevateToSwitchLow.whenPressed(new ElevateToSwitch("Low"));		
+		elevateToSwitchHigh.whenPressed(new ElevateToSwitch("High"));	
+		zeroElevator.whenPressed(new ZeroElevator());	
+		levelTwoElevate.whenPressed(new IntakeFromPyramid("level 2"));	
+		levelThreeElevate.whenPressed(new IntakeFromPyramid("level 3"));	
+		manualElevate.whenPressed(new ManualElevate());	
 
-	//// TRIGGERING COMMANDS WITH BUTTONS
-	// Once you have a button, it's trivial to bind it to a button in one of
-	// three ways:
-
-	// Start the command when the button is pressed and let it run the command
-	// until it is finished as determined by it's isFinished method.
-	// button.whenPressed(new ExampleCommand());
-
-	// Run the command while the button is being held down and interrupt it once
-	// the button is released.
-	// button.whileHeld(new ExampleCommand());
-
-	// Start the command when the button is released and let it run the command
-	// until it is finished as determined by it's isFinished method.
-	// button.whenReleased(new ExampleCommand());
-}
+		
+	}
+	
+	public boolean manualElevatePressed() {
+		return manualElevate.get();
+	}
+	
+	

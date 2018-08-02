4885477f1223fc07b0f0c18d7d58e5f8de6d7625
cvsooraj3 <35387667+cvsooraj3@users.noreply.github.com>
2018-08-02 15:28:43 -0700

    Added Constants.java

---
+++ src/org/usfirst/frc/team115/robot/Constants.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team115.robot;
+
+public class Constants {
+
+	
+	public static int kTimeoutMs;
+	public static int kPIDLoopIdx;
+	
+	
+	
+	// switch heights
+	public static double kSwitchDefault;
+	public static double kSwitchHigh;
+	
+	//intake From pyramid
+	public static double kLevelTwo;
+	public static double kLevelThree;
+
+	
+			
+}

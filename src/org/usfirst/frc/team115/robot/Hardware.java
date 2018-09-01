5ca5ae0cba3605eab274ba345c4253cb4cb17330
TangyKiwi <orange.panda.65@gmail.com>
2018-08-31 17:23:26 -0700

    Update Code

---
+++ src/org/usfirst/frc/team115/robot/Hardware.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team115.robot;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class Hardware 
+{
+	public static TalonSRX left;
+	public static TalonSRX right;
+	public static TalonSRX rot;
+}
--- src/org/usfirst/frc/team115/robot/subsystems/Intake.java
+++ src/org/usfirst/frc/team115/robot/subsystems/Intake.java
@@ -1,28 +1,70 @@
 package org.usfirst.frc.team115.robot.subsystems;
 
+import org.usfirst.frc.team115.robot.Constants;
+import org.usfirst.frc.team115.robot.Hardware;
 import org.usfirst.frc.team115.robot.commands.IntakeCommand;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Intake extends Subsystem 
 {
 	private DoubleSolenoid intake;
-	private TalonSRX left;
-	private TalonSRX right;
-	private TalonSRX rot;
 	public DigitalInput breakbeam;
-
+	public double setPoint;
+	public double rotations;
+	int state;
+	
 	public Intake()  
 	{
-		left = new TalonSRX(16);	//left cantalon port tbd
-		right = new TalonSRX(17);   //right cantalon port tbd
-		rot = new TalonSRX(18);     //rot cantalon port tbd
+		state = 0; //0 = disabled, 1 = rotating out, 2 = rotating in, 3 = hold
+		Hardware.left = new TalonSRX(Constants.talonLeftIntake);	//left cantalon port tbd
+		Hardware.right = new TalonSRX(Constants.talonRightIntake);   //right cantalon port tbd
+		Hardware.rot = new TalonSRX(Constants.talonRotateIntake);     //rot cantalon port tbd
+		
+		/* first choose the sensor */
+		Hardware.rot.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, Constants.kPIDLoopIdx, Constants.kTimeoutMs);
+		Hardware.rot.setSensorPhase(true);
+		Hardware.rot.setInverted(false);
+
+		/* Set relevant frame periods to be at least as fast as periodic rate */
+		Hardware.rot.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, Constants.kTimeoutMs);
+		Hardware.rot.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, Constants.kTimeoutMs);
+
+		/* set the peak and nominal outputs */
+		Hardware.rot.configNominalOutputForward(0, Constants.kTimeoutMs);
+		Hardware.rot.configNominalOutputReverse(0, Constants.kTimeoutMs);
+		Hardware.rot.configPeakOutputForward(0.5, Constants.kTimeoutMs);
+		Hardware.rot.configPeakOutputReverse(-0.5, Constants.kTimeoutMs);
+
+		/* set closed loop gains in slot0 - see documentation */
+		Hardware.rot.selectProfileSlot(Constants.kSlotIdx, Constants.kPIDLoopIdx);
+		Hardware.rot.config_kF(0, 0.2, Constants.kTimeoutMs);
+		Hardware.rot.config_kP(0, 0.2, Constants.kTimeoutMs);
+		Hardware.rot.config_kI(0, 0, Constants.kTimeoutMs);
+		Hardware.rot.config_kD(0, 0, Constants.kTimeoutMs);
+		/* set acceleration and vcruise velocity - see documentation */
+		Hardware.rot.configMotionCruiseVelocity(15000, Constants.kTimeoutMs);
+		Hardware.rot.configMotionAcceleration(6000, Constants.kTimeoutMs);
+		/* zero the sensor */
+		Hardware.rot.setSelectedSensorPosition(0, Constants.kPIDLoopIdx, Constants.kTimeoutMs);
+		
+		new Notifier(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				loop();
+			}
+		}).startPeriodic(0.005);
 	}
 
 	public void extendIntake() 
@@ -37,32 +79,61 @@ public class Intake extends Subsystem
 	
 	public void intakeCube() 
 	{
-		left.set(ControlMode.PercentOutput, -0.65);
-		right.set(ControlMode.PercentOutput, 0.65);
+		Hardware.left.set(ControlMode.PercentOutput, -0.65);
+		Hardware.right.set(ControlMode.PercentOutput, 0.65);
 		//Robot.carriage.intakeCube(-0.45); //use if carriage is needed
 	}
 	
 	public void outtakeCube() 
 	{
-		left.set(ControlMode.PercentOutput, 0.65);
-		right.set(ControlMode.PercentOutput, -0.65);
+		Hardware.left.set(ControlMode.PercentOutput, 0.65);
+		Hardware.right.set(ControlMode.PercentOutput, -0.65);
 		//Robot.carriage.outtakeCube(-1); //use if carriage is needed
 	}
 	
 	public void rotateIn()
 	{
-		rot.set(ControlMode.PercentOutput, 0.65);
+		setPoint = 4096 * rotations;
+		Hardware.rot.set(ControlMode.MotionMagic, setPoint);
 	}
 	
 	public void rotateOut()
 	{
-		rot.set(ControlMode.PercentOutput, -0.65);
+		setPoint = -4096 * rotations;
+		Hardware.rot.set(ControlMode.MotionMagic, setPoint);
+	}
+	
+	public void loop()
+	{
+		if(state == 1)
+		{
+			if(Hardware.rot.getSelectedSensorPosition(0) > Constants.lowBufferIntakeOut)
+			{
+				stop();
+			}
+			else
+			{
+				Hardware.rot.set(ControlMode.MotionMagic, setPoint);
+			}
+		}
+		else if(state == 2)
+		{
+			if(Hardware.rot.getSelectedSensorPosition(0) < Constants.lowBufferIntakeIn)
+			{
+				stop();
+			}
+			else
+			{
+				Hardware.rot.set(ControlMode.MotionMagic, setPoint);
+			}
+		}
 	}
 	
 	public void stop()  
 	{
-		left.set(ControlMode.PercentOutput, 0);
-		right.set(ControlMode.PercentOutput, 0);
+		Hardware.left.set(ControlMode.PercentOutput, 0);
+		Hardware.right.set(ControlMode.PercentOutput, 0);
+		Hardware.rot.set(ControlMode.PercentOutput, 0);
 	}
 	
 	public void initDefaultCommand()  
